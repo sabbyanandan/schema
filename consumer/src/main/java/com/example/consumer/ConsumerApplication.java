31d90223daa9357b475086669e4555ba5703e671
Sabby Anandan <sabby.anandan@gmail.com>
2018-03-15 14:23:49 -0700

    A working sample of producerv1 -> consumerv2

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -6,28 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.schema.avro.AvroSchemaMessageConverter;
 import org.springframework.cloud.stream.schema.client.EnableSchemaRegistryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.util.MimeType;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
 @EnableSchemaRegistryClient
 public class ConsumerApplication {
 
-	@Bean
-	public MessageConverter userMessageConverter() {
-		return new AvroSchemaMessageConverter(MimeType.valueOf("application/*+avro"));
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
 	@StreamListener(Sink.INPUT)
-	public void process(Sensor data){
+	public void process(Sensor data) {
 		System.out.println(data);
 	}
 }
